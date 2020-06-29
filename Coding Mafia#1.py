e59943d9e4cc5887a89260c35ca27db9590ad999
firefistacez <57040776+firefistacez@users.noreply.github.com>
2020-06-29 13:14:33 +0530

    initial commit

---
+++ Coding Mafia#1.py
@@ -0,0 +1,22 @@
+limit = int(input("Input:"))
+input_numbers = input("")
+userList = input_numbers.split()
+number = 1
+last = len(userList)
+answerList = []
+for i in range(last-1):
+    answer = 0
+    if i<5:
+        for j in range(i+1):
+            answer = answer + int(userList[j])
+        answer = answer/number
+        answerList.append(round(answer,4))
+        if number < 5:
+            number += 1
+    else: 
+        for j in range(i-4,i+1):
+            answer = answer + int(userList[j])
+        answer = answer/number
+        answerList.append(round(answer,4))
+
+print(answerList)
---
+++ README.md
@@ -0,0 +1,6 @@
+You are given a data stream terminated by -1 and the size of the sliding windows.For each variation in sliding window you need to tell the average of data in current sliding window.Print 4 digits after the decimal point.
+
+Input: 5
+        51 62 24 51 79 33 78 78 84 42 -1
+Output: 51.0 56.5 45.6667 47.0 53.4 49.8 51.8 62.6 69.2 61.8
+.
